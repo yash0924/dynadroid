aa2aeafa61ac6f332179929b1b0d06ac4ff8263d
pete.sharum@gmail.com <pete.sharum@gmail.com@b11a92f0-8ab4-2b13-9011-6a5a0c52aeb5>
2011-08-26 22:39:14 +0000

    Refactored HttpCall.java  - Added support for PUT and DELETE  - Added support for cache tagging    . Allow API consumer to specify tags to differentiate results from same URL (ex: paging handled in headers)  - Expose full HttpResponse object  - Fixed header bug: moved variable from class to instance scope  - DRYed up actions  - Added HttpDelegate functionality (see HttpDelegateTask) for package members to inject behavior before complete() is called

--- trunk/java/src/org/dynadroid/utils/HttpDelegate.java
+++ trunk/java/src/org/dynadroid/utils/HttpDelegate.java
@@ -1,5 +1,20 @@
 package org.dynadroid.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class HttpDelegate {
+    private List<HttpDelegateTask> completeTasks = new ArrayList<HttpDelegateTask>();
+
     public abstract void completed(HttpCall httpCall);
+
+    void addCompleteTask(HttpDelegateTask task) {
+        completeTasks.add(task);
+    }
+    void complete(HttpCall httpCall) {
+        for (HttpDelegateTask task : completeTasks) {
+            task.doTask();
+        }
+        this.completed(httpCall);
+    }
 }
---
+++ trunk/java/src/org/dynadroid/utils/HttpDelegateTask.java
@@ -0,0 +1,12 @@
+package org.dynadroid.utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: psharum
+ * Date: 8/25/11
+ * Time: 1:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface HttpDelegateTask {
+    public void doTask();
+}
